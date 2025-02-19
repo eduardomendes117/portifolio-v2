b7fe4d52ae05713e620edff917aac84f1adb5119
Eduardo Mendes <107651217+eduardomendes117@users.noreply.github.com>
2025-02-19 13:22:29 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizeCss: true,
+  },
+  compiler: {
+    removeConsole: true,
+  },
 };
 
 export default nextConfig;
